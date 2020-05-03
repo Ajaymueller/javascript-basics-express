d5edcb5e0c4ce2c28f4b860cc71eb9b45e10565e
Aidan Mueller <Aidan_Mueller@hotmail.co.uk>
2020-05-03 18:19:07 +0100

    added a public folder with index and about page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable prefer-destructuring */
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join('public')));
+
+const members = {
+  id: 1,
+  name: 'John',
+  email: 'John@email.com',
+  status: 'active',
+};
+
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
 
 const stringsRouter = require('../routes/stringsRouter');
 const numbersRouter = require('../routes/numbersRouter');
